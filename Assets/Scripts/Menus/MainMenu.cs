2b38f432a81d07d0157e7d8db25a0fc48b749874
Ethan <findwaldoFTLOG@gmail.com>
2022-07-16 21:50:54 -0400

    Main menu, WIP: level select

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -2,11 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     public GameObject mainMenuPanel;
     public GameObject levelSelectPanel;
+
+    [Space]
+
+    public Slider musicSlider;
+    public Slider effectsSlider;
+
+    private void Awake() {
+        mainMenuPanel.SetActive(true);
+        levelSelectPanel.SetActive(false);
+    }
 
     public void StartGame() {
         // Assuming the second scene in the build index is the first level we're all good.
@@ -20,6 +31,14 @@ public class MainMenu : MonoBehaviour
 
     public void Quit() {
         Application.Quit();
+    }
+
+    public void SetMusicVolume(float volume) {
+        // TODO: Change Wwise music volume
+    }
+
+    public void SetEffectsVolume(float volume) {
+        // TODO: Change Wwise effects volume
     }
 
     // TODO: Volume sliders
