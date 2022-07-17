f2cfb1aa00efe2dde1da820596b3335efb1fa4d8
Ethan <findwaldoFTLOG@gmail.com>
2022-07-16 21:15:28 -0400

    Add prefab changes from previous fix

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -1,8 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    
+    public GameObject mainMenuPanel;
+    public GameObject levelSelectPanel;
+
+    public void StartGame() {
+        // Assuming the second scene in the build index is the first level we're all good.
+        SceneManager.LoadScene(1);
+    }
+
+    public void SetLevelSelectVisible(bool visible) {
+        mainMenuPanel.SetActive(!visible);
+        levelSelectPanel.SetActive(visible);
+    }
+
+    public void Quit() {
+        Application.Quit();
+    }
+
+    // TODO: Volume sliders
 }
