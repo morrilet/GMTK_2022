b4d83a44a612b8c71feb7a1c38d737310c24047c
Ethan <findwaldoFTLOG@gmail.com>
2022-08-02 00:39:41 -0400

    Add door inversion

--- Assets/Scripts/Tiles/Door.cs
+++ Assets/Scripts/Tiles/Door.cs
@@ -6,23 +6,43 @@ public class Door : MonoBehaviour, ITriggerable
 {
     public GameObject modelObject;
     public LayerMask blockingMask;
+    public bool isInverted;
+
+    private void Awake() {
+        if (isInverted)
+            modelObject.SetActive(false);
+    }
  
     // Close the door.
     public IEnumerator releaseTriggerAction() {
+        if (!isInverted)
+            Close();
+        else 
+            Open();
+        yield return null;
+    }
+
+    // Open the door.
+    public IEnumerator triggerAction() {
+        if (!isInverted)
+            Open();
+        else 
+            Close();
+        yield return null;
+    }
+
+    private void Open() {
+        modelObject.SetActive(false);
+        AudioManager.PlaySound(GlobalVariables.DOOR_OPEN_EFFECT);
+    }   
+    
+    private void Close() {
         if (CanClose()) {
             modelObject.SetActive(true);
             AudioManager.PlaySound(GlobalVariables.DOOR_CLOSE_EFFECT);
         }
         else  // We could try to be smart here and not re-queue if the door should open again, but since requeues happen before real actions we're fine. It's fine.
             WorldController.instance.RequeueActionForNextTurn(releaseTriggerAction);
-        yield return null;
-    }
-
-    // Open the door.
-    public IEnumerator triggerAction() {
-        modelObject.SetActive(false);
-        AudioManager.PlaySound(GlobalVariables.DOOR_OPEN_EFFECT);
-        yield return null;
     }
 
     /// <summary>
